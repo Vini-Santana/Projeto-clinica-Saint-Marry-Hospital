cd6fd76ba0a0530516f3dbddc04403853aec3203
Vinicius Vanderlei Santana <vinisantana1112@gmail.com>
2024-10-25 21:59:24 -0300

    2-Tela Login criada modelo de banco de dados atualizado

--- app/login.tsx
+++ app/login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from "react-native";
 
 export default function App() {
   const [user, setUser] = useState(null);
@@ -7,64 +7,85 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {/* Imagem precisa estar dentro do JSX retornado */}
       <Image 
         source={require('../assets/images/Logo-icon.png')} 
         style={styles.image} 
       />
       <Text style={styles.title}>Login</Text>
-      <Text> Email</Text>
+
+      <Text style={styles.text}>Email</Text>
       <TextInput
         style={styles.input}
         onChangeText={setUser}
         value={user}
         placeholder="Digite seu email"
-        // keyboardType="numeric"
+        placeholderTextColor={"#D9D9D9"}
+
       />
-      <Text>Senha</Text>
+
+      <Text style={styles.text}>Senha</Text>
       <TextInput
         style={styles.input}
         onChangeText={setSenha}
         value={senha}
         placeholder="Digite sua senha"
+        placeholderTextColor={"#D9D9D9"}
         secureTextEntry={true}
-        marginTop= {10}
+
       />
 
+      <TouchableOpacity style={styles.SignInButton}>
+        <Text style={styles.SignInButtonText}>Entrar</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.text}>Criar cadastro</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
-  image:{
-    width: 200,
-    height: 200,
+  image: {
+    width: 150,
+    height: 150,
     marginTop: 50,
   },
-
+  
   title: {
-    fontSize: 40,
-    fontWeight: "bold",
-    color: "#333",
+    fontSize: 44,
     fontFamily: "Montserrat",
     marginTop: 20,
     marginBottom: 20,
   },
-  
   input: {
-    width: 200,
+    width: 272,
     height: 40,
-    borderColor: "#333",
     borderWidth: 1,
-    borderRadius: 5,
+    borderRadius: 8,
     padding: 10,
-    textAlign: "center",
+    textAlign: "left",
+    marginBottom: 10, 
+    borderColor: "#D9D9D9",
+  },
+  
+  SignInButton: {
+    width: 272,
+    backgroundColor: "#000000",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
+    margin: 10,
+  },
+  SignInButtonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  text: {
+    width: 272,
+    textAlign: "left", // Alinha o texto à esquerda
+    marginBottom: 5, // Dá um pequeno espaço entre o texto e o campo de input
   },
   container: {
     flex: 1,
-    // justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f0f0f0",
   },
 });
